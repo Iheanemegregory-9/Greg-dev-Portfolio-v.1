5d185a649360b0a7686fc5bc4b6e63feb7addf01
gregdev <iheanemegregory9@gmail.com>
2022-03-31 22:31:01 +0100

    alert.js added

---
+++ alert.js
@@ -0,0 +1,7 @@
+let info = prompt('This is a development project. Just type "YES" to continue', '')
+
+if(info == 'YES'){
+    alert('Click "OK" to view the page')
+}else{
+    alert('You typed the wrong key word, its "YES", relaod the page and try again')
+}

6b801c06aeb352739d260997ca25e0c4642f90a7
gregdev <iheanemegregory9@gmail.com>
2022-03-31 23:30:58 +0100

    spellings corrected

--- alert.js
+++ alert.js
@@ -3,5 +3,5 @@ let info = prompt('This is a development project. Just type "YES" to continue', 
 if(info == 'YES'){
     alert('Click "OK" to view the page')
 }else{
-    alert('You typed the wrong key word, its "YES", relaod the page and try again')
+    alert('You typed the wrong key word, its "YES", reload the page and try again')
 }
